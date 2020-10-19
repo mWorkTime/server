2b8876fc8d6fb4a0fe450444105da77f22c41625
illia <shabelnyk.ilya@gmail.com>
2020-10-19 15:58:30 +0300

    feat(auth): add logout to auth.service. Add field 'redirectToMain' to user reducer. Add action clearUserData to types, reducer, action. Add function for logout from app to dashboard. Add axios interceptor for request (make all request with header authorization)

--- services/auth.service.js
+++ services/auth.service.js
@@ -87,6 +87,7 @@ exports.getUserAndLogin = (email, password, expiresTime, res) => {
         orgName: user.organization.name,
         orgCode: user.organization.code
       }, { expiresIn: expiresTime })
+      res.cookie('token', token, { expiresIn: expiresTime })
 
       res.status(200).json({
         token,
